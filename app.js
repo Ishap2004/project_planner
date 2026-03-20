2ecbbaa3a3b1f190b50a262cae73f447d932ad85
ishaR0809 <ishaspatel089@gmail.com>
2026-03-19 23:04:58 -0400

    Final project update

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
+// require('dotenv').config();
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var app = express();
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var tasksRouter = require('./routes/tasks');
+
+// const db = require('./config/database');
+
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/tasks', tasksRouter);
+
+// module.exports = app;
+
+
 require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var app = express();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var tasksRouter = require('./routes/tasks');
+const app = express();
 
-const db = require('./config/database');
+// Database connection
+require('./config/database');
 
+// Import Routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 
-module.exports = app;
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
 
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+module.exports = app;
