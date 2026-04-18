6d5914959b60ce5b3639faf7f565aaceae9b5377
ishaR0809 <ishaspatel089@gmail.com>
2026-04-17 20:19:55 -0400

    Fix Render deployment to serve compiled React application

--- app.js
+++ app.js
@@ -51,10 +51,7 @@ app.use(express.urlencoded({ extended: false })); // parse form data
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Test route 
-app.get('/', (req, res) => {
-  res.send('🚀 Student Life Planner API is running...');
-});
+
 
 // DEMO HIGHLIGHT: This is our Backend Express Server.
 // Here we define the main API routes that listen for requests coming from our Frontend.
